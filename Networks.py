b33658d713d1d5bb21a1d3e2b6956c0fa330c766
Hudson-Liu <96438596+Hudson-Liu@users.noreply.github.com>
2022-06-07 14:08:01 -0400

    Bug Fixes: Functional Model WB Randomizer
    (WB stands for Weight and Bias)
    I fixed a ton of bugs after shoving my head into a refrigerator so that I could actually think clearly during the hot summer.

--- Networks.py
+++ Networks.py
@@ -11,27 +11,31 @@ import random
 import numpy as np
 
 batch_size = 1 #only one batch since there's no gradient descent so batch size doesn't really matter
-nlp_features = 1 #only 1 feature, being the individual character
+nlp_features = 97 #97 features because one hot encoding
 nlp_embedding = 20 #just a random constant i decided on
-nlp_nodes = 60
+nlp_nodes = 50
 nlp_dense = 50
+anet_nodes = 50
 anet_pred_vars = 5 #All the parameters that anet can change
 anet_dense = 50 #just for some extra complexity
 hnet_pred_vars = 10 #same as above
 hnet_dense = 50
+hnet_first_layer = 50
 
 nlp = keras.Sequential(name="Natural Langauge Processor")
 nlp.add(keras.layers.LSTM(units = nlp_nodes, batch_input_shape = (batch_size, None, nlp_features))) #when 1 it is 4 when 4 it is 16
 nlp.add(keras.layers.Dense(units = nlp_dense, activation = "relu"))
 nlp.add(keras.layers.Dense(units = nlp_embedding, activation = "relu"))
+oobama = nlp.get_weights()
 
 anet = keras.Sequential(name="Architecture Network")
-anet.add(keras.layers.LSTM(units = nlp_embedding, activation = "relu", stateful = True, batch_input_shape = (batch_size, None, nlp_embedding)))
+anet.add(keras.layers.LSTM(units = anet_nodes, activation = "relu", stateful = True, batch_input_shape = (batch_size, None, nlp_embedding)))
 anet.add(keras.layers.Dense(units = anet_dense))
 anet.add(keras.layers.Dense(units = anet_pred_vars))
+obama = anet.get_weights()
 
 hnet = keras.Sequential(name="Hyperparameter Network")
-hnet.add(keras.layers.Dense(units = nlp_embedding))
+hnet.add(keras.layers.Dense(units = hnet_first_layer, input_shape=(nlp_embedding,)))
 hnet.add(keras.layers.Dense(units = hnet_dense))
 hnet.add(keras.layers.Dense(units = hnet_pred_vars))
 
@@ -62,7 +66,7 @@ for data in nl_removed:
     one_hot_encoded.append(desc_encoded)
 
 #generate random weights and biases for LSTM layer
-def rand_lstm_weights(lstm_nodes, n_dense):
+def rand_lstm_weights(input_shape, lstm_nodes, n_dense):
     wb = [] #wb is for weights and biases
 
     #randomly set values of lstm nodes
@@ -70,12 +74,17 @@ def rand_lstm_weights(lstm_nodes, n_dense):
     U1 = np.array(U, dtype=float)
     b = [(20 * random.random() - 10) for i in range(0, lstm_nodes * 4)] #bias can be any value
     b1 = np.array(b, dtype=float)
-    W = [[(20 * random.random() - 10) for i in range(0, lstm_nodes * 4)]] #weights can be any value
+    W = [[(20 * random.random() - 10) for i in range(0, lstm_nodes * 4)] for i in range(0, input_shape)] #weights can be any value
     W1 = np.array(W, dtype=float)
     wb.append(W1)
     wb.append(U1)
     wb.append(b1)
-    
+
+    wb = rand_dense_weights(lstm_nodes, n_dense, wb)
+
+    return wb
+
+def rand_dense_weights(input_shape, n_dense, wb = []):
     #sets weights and biases
     i = 0
     for layer in n_dense:
@@ -83,14 +92,14 @@ def rand_lstm_weights(lstm_nodes, n_dense):
         if i != 0:
             weights = np.array([[(20 * random.random() - 10) for i in range(0, layer)] for l in range(0, n_dense[i-1])], dtype=float)
         else: 
-            weights = np.array([[(20 * random.random() - 10) for i in range(0, layer)] for l in range(0, lstm_nodes)], dtype=float)
+            weights = np.array([[(20 * random.random() - 10) for i in range(0, layer)] for l in range(0, input_shape)], dtype=float)
         wb.append(weights)
 
         #there is no input layer (lstm directly feeds to dense) and output layer has biases
         biases = np.array([(20 * random.random() - 10) for i in range(0, layer)], dtype=float)
         wb.append(biases)
         i += 1
-
+    
     return wb
 
 #tweaks the best individuals's weights and biases by up to 1
@@ -104,11 +113,11 @@ def tweaker(wb):
     return wb
 
 #initialize the weights and biases of the nlp, anet, and hnet randomly
-nlp_wb = rand_lstm_weights(lstm_nodes = nlp_nodes, n_dense = [nlp_dense, nlp_embedding])
+nlp_wb = rand_lstm_weights(input_shape = nlp_features, lstm_nodes = nlp_nodes, n_dense = [nlp_dense, nlp_embedding])
 nlp.set_weights(nlp_wb)
-anet_wb = rand_lstm_weights(lstm_nodes = nlp_embedding, n_dense = [anet_dense, anet_pred_vars])
+anet_wb = rand_lstm_weights(input_shape = nlp_embedding, lstm_nodes = anet_nodes, n_dense = [anet_dense, anet_pred_vars])
 anet.set_weights(anet_wb)
-hnet_wb = rand_lstm_weights(lstm_nodes = nlp_embedding, n_dense = [hnet_dense, hnet_pred_vars])
+hnet_wb = rand_dense_weights(input_shape = nlp_embedding, n_dense = [hnet_first_layer, hnet_dense, hnet_pred_vars])
 hnet.set_weights(hnet_wb)
 
 #forward propagation
