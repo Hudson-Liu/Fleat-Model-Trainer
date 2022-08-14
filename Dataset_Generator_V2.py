06ff9508e2351409c34683d8cf1227cb0ae0cebf
Hudson-Liu <96438596+Hudson-Liu@users.noreply.github.com>
2022-08-14 19:07:13 -0400

    A bunch of files for the newer HYPT project

---
+++ Dataset_Generator_V2.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  6 20:53:27 2022
+
+@author: Joe Mama
+"""
+
+from tkinter import E
+import keras
+import cv2
+import numpy as np
+import pickle
+import tensorflow as tf
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
+
+# TODO Eventually, this whole program should be converted into a class, and these variables should be set in the driver program (__main__.py) that runs both networkv2 and dataset generator
+# we'll also haev a cool __version__.py file
+num_images_main = 100
+num_images_sub = 1000  # change to none when you have more memory
+resolution = [224, 224]
+
+
+class PreProcessor:
+    """An image preprocessor, used to preprocess all the images in a batch"""
+
+    # TODO This should also eventually go in __main__.py
+    alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ "
+
+    def __init__(self, num_images, resolution, use_labels):
+        self.num_images = num_images
+        self.resolution = resolution
+        self.use_labels = use_labels
+
+    # The only part that needs to change to convert this from somethign that only takes keras datasets to something that can accept any input dataset is importing image files and converting them into a np array and also grayscaling
+    def preprocess_images(self, x, y, rgb):
+        # Splice images
+        x = x[0:self.num_images]
+        y = y[0:self.num_images]
+
+        # Scales images to 64x64 resolution and makes image greyscale
+        x_resized = []
+        for img in x:
+            # if it's enlargening
+            if img.shape[0] <= self.resolution[0] and img.shape[1] <= self.resolution[1]:
+                interpolation = cv2.INTER_CUBIC
+            else:  # if it's downscaling
+                interpolation = cv2.INTER_AREA
+            resized = cv2.resize(img, dsize=(
+                self.resolution[0], self.resolution[1]), interpolation=interpolation)
+            if rgb:
+                resized = cv2.cvtColor(resized, cv2.COLOR_RGB2GRAY)
+            x_resized.append(resized)
+
+        # Normalize
+        x_resized = np.array(x_resized)
+        x_resized = x_resized.astype("float32") / 255.0
+
+        # Make sure images have shape (224, 224, 1)
+        x_resized = np.expand_dims(x_resized, -1)
+
+        # One hot encoding labels
+        if self.use_labels:
+            y_num = np.array(to_categorical(y)) #entire label
+            dataset = [x_resized, y_num]
+            return dataset
+        else:
+            return x_resized
+
+
+main_preproc = PreProcessor(num_images_main, resolution, False)
+sub_preproc = PreProcessor(num_images_sub, resolution,  True)
+datasets_main = {}
+datasets_sub = {}
+
+# Fashion MNIST
+(train_img, train_labels), (test_img,
+                            test_labels) = tf.keras.datasets.fashion_mnist.load_data()
+imgs = np.concatenate((train_img, test_img), axis=0)
+labels = np.concatenate((train_labels, test_labels), axis=0)
+datasets_main.update({"fashion-mnist" : main_preproc.preprocess_images(imgs, labels, False)})
+datasets_sub.update({"fashion-mnist" : sub_preproc.preprocess_images(imgs, labels, False)})
+
+# CIFAR 10
+(train_img, train_labels), (test_img,
+                            test_labels) = tf.keras.datasets.cifar10.load_data()
+imgs = np.concatenate((train_img, test_img), axis=0)
+labels = np.concatenate((train_labels, test_labels), axis=0)
+datasets_main.update({"cifar10" : main_preproc.preprocess_images(imgs, labels, True)})
+datasets_sub.update({"cifar10" : sub_preproc.preprocess_images(imgs, labels, True)})
+
+# CIFAR 100
+(train_img, train_labels), (test_img,
+                            test_labels) = tf.keras.datasets.cifar100.load_data()
+imgs = np.concatenate((train_img, test_img), axis=0)
+labels = np.concatenate((train_labels, test_labels), axis=0)
+datasets_main.update({"cifar100" : main_preproc.preprocess_images(imgs, labels, True)})
+datasets_sub.update({"cifar100" : sub_preproc.preprocess_images(imgs, labels, True)})
+
+# MNIST
+(train_img, train_labels), (test_img, test_labels) = tf.keras.datasets.mnist.load_data()
+imgs = np.concatenate((train_img, test_img), axis=0)
+labels = np.concatenate((train_labels, test_labels), axis=0)
+datasets_main.update({"mnist" : main_preproc.preprocess_images(imgs, labels, False)})
+datasets_sub.update({"mnist" : sub_preproc.preprocess_images(imgs, labels, False)})
+
+# Save all datasets
+with open("datasets", "wb") as fp:  # Pickling
+    pickle.dump(datasets_main, fp)
+
+with open("datasets_optimization", "wb") as fp:  # Pickling
+    pickle.dump(datasets_sub, fp)
---
+++ NetworksV3.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  10 20:14:12 2022
+
+Hopefully, unlike last time, this actually goes well
+
+@author: Joe Mama
+"""
+
+import keras
+from keras.layers import TimeDistributed, Conv2D, Dense, MaxPooling2D, Flatten, LSTM, Concatenate
+import pickle
+import tensorflow as tf
+import time
+import numpy as np
+
+# CNN-RNN/CNN-LSTM for processing images and corresponding answers
+# Turn this into a global variable thing later
+num_images = 100  # images per timestep
+resolution = [224, 224]
+TRAIN_TEST_SPLIT = 0.2
+alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ "
+BATCH_SIZE = 1
+NUM_OPTIMIZERS = 8 # Number of unique optimizers being used in "Optimized_Parameters.py"
+
+# Image processing
+# shape=(timesteps,resolution,resolution,rgb channels)
+img_input = keras.Input(shape=(num_images, resolution[0], resolution[1], 1))
+x = TimeDistributed(Conv2D(filters=8, kernel_size=(3, 3), padding='same', activation='relu'))(img_input) #Full scale model has 64 filters for both
+x = TimeDistributed(Conv2D(filters=8, kernel_size=(3, 3), padding='same', activation='relu'))(x)
+x = TimeDistributed(MaxPooling2D(pool_size=(2, 2), strides=2))(x)
+#filters_convs = [(128, 2), (256, 3), (512, 3), (512, 3)] #Full scale model, my computer doesn't have enough GPU memory for it
+filters_convs = [(8, 2), (8, 3), (8, 3), (8, 3)] #Reduced scale model, easier on GPU memory
+for n_filters, n_convs in filters_convs:
+    for _ in range(n_convs):
+        x = TimeDistributed(Conv2D(filters=n_filters, kernel_size=(3, 3), padding='same', activation='relu'))(x)
+    x = TimeDistributed(MaxPooling2D(pool_size=(2, 2), strides=2))(x)
+x = TimeDistributed(Flatten())(x)
+x = TimeDistributed(Dense(units=100), name='Image_Preprocessing')(x)
+x = LSTM(units=100)(x)
+dataset_embed = Dense(units=100, activation='relu', name='Image_Embed')(x)
+
+# Model properties embedding
+properties_input = keras.Input(shape=(NUM_OPTIMIZERS + 1))
+properties_embed = Dense(units=100, name='Model_Properties_Embed')(properties_input)
+
+# Combines both models
+merge = Concatenate(axis=1, name="Total_Embed")([dataset_embed, properties_embed])
+
+# learning rate output
+x = Dense(units=50, activation='relu')(merge)
+output = Dense(units=1, name='Learning_Rate', activation='relu')(x)
+
+# This model covers all the inputs and outputs
+model = keras.Model([img_input, properties_input], output)
+
+# Import and preprocess data
+print("Started importing data")
+start = time.time()
+
+#directory = "/kaggle/input/dataset/" #for kaggle
+directory = "" #for local
+with open(f"{directory}datasets", "rb") as fp:
+    datasets = pickle.load(fp)
+    
+with open(f"{directory}model_runs_RUN_0", "rb") as fp:
+    true_rates = pickle.load(fp)
+
+#Normalize Parameter numbers
+param_num = [i[0] for i in true_rates]
+normalized_param = [(i - min(param_num))/(max(param_num) - min(param_num)) for i in param_num]
+
+#One Hot Encodes the optimizer
+all_optimizers = [i[1] for i in true_rates]
+KNOWN_OPTIMIZERS = ["SGD", "RMSprop", "Adam", "Adadelta", "Adagrad", "Adamax", "Nadam", "Ftrl"]
+encoded = []
+for i in all_optimizers:
+    found = -1
+    for index, j in enumerate(KNOWN_OPTIMIZERS):
+        if i == j:
+            found = index
+            break
+    if found == -1:
+        raise ValueError("Invalid optimizer cannot be One-Hot-Encoded")
+    else:
+        onehot = [0 for _ in KNOWN_OPTIMIZERS]
+        onehot[found] = 1
+        encoded.append(onehot)
+
+#Concatenates encoded with parameter numbers
+assert len(encoded) == len(normalized_param)
+for index, element in enumerate(encoded):
+    element.append(normalized_param[index])
+
+#Concatenates all the features together to produce "x"
+#dataset_types = [i[2] for i in true_rates] #TODO this is also fro the proper output
+dataset_types = ["fashion-mnist", "cifar10", "cifar100", "mnist", "fashion-mnist", "cifar10", "cifar100", "mnist", "fashion-mnist", "cifar10", "cifar100", "mnist", "fashion-mnist", "cifar10", "cifar100", "mnist", "fashion-mnist", "cifar10"]
+images = np.array([datasets[l] for l in dataset_types])
+encoded = np.array(encoded)
+
+#Create x and y
+x = [images, encoded]
+#y = [i[3] for i in true_rates] #TODO this is for the proper modified output of optimized parameters
+y = np.array([i[2] for i in true_rates])
+
+elapsed = round(time.time() - start, 3)
+print(f"Finished importing data || Elapsed Time: {elapsed}s")
+
+#Just add some more preprocessing and a model.fit and you'll be done
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), loss="mse")
+model.fit(x=x, y=y, epochs=10, batch_size=BATCH_SIZE)
