278c2e6b58d7af9785aa10a36c869e6cf668c01a
Hudson-Liu <96438596+Hudson-Liu@users.noreply.github.com>
2022-08-19 09:38:15 -0400

    Full refactor & added in generator
    I fully refactored the program to work as 2 classes, and I also added in a new class that sequentially imports the dataset, which makes the possible training dataset size much much larger. This program still hasn't been fully tested, so I'd recommend analyzing the outputs of the generator later on to make sure it's properly working.

--- NetworksV3.py
+++ NetworksV3.py
@@ -14,168 +14,234 @@ import tensorflow as tf
 import time
 import numpy as np
 import datetime
-from tqdm import tqdm
+import config
+import math
+import random
 
-# CNN-RNN/CNN-LSTM for processing images and corresponding answers
-# Turn this into a global variable thing later
-num_images = 100  # images per timestep
-resolution = [224, 224]
-TRAIN_TEST_SPLIT = 0.2
-alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ "
-BATCH_SIZE = 1
-NUM_OPTIMIZERS = 8 # Number of unique optimizers being used in "Optimized_Parameters.py"
-
-# Image processing
-# shape=(timesteps,resolution,resolution,rgb channels)
-img_input = keras.Input(shape=(num_images, resolution[0], resolution[1], 1))
-x = TimeDistributed(Conv2D(filters=8, kernel_size=(3, 3), padding='same', activation='relu'))(img_input) #Full scale model has 64 filters for both
-x = TimeDistributed(Conv2D(filters=8, kernel_size=(3, 3), padding='same', activation='relu'))(x)
-x = TimeDistributed(MaxPooling2D(pool_size=(2, 2), strides=2))(x)
-#filters_convs = [(128, 2), (256, 3), (512, 3), (512, 3)] #Full scale model, my computer doesn't have enough GPU memory for it
-filters_convs = [(8, 2), (8, 3), (8, 3), (8, 3)] #Reduced scale model, easier on GPU memory
-for n_filters, n_convs in filters_convs:
-    for _ in range(n_convs):
-        x = TimeDistributed(Conv2D(filters=n_filters, kernel_size=(3, 3), padding='same', activation='relu'))(x)
-    x = TimeDistributed(MaxPooling2D(pool_size=(2, 2), strides=2))(x)
-x = TimeDistributed(Flatten())(x)
-x = TimeDistributed(Dense(units=50), name='Image_Preprocessing')(x)
-x = LSTM(units=50)(x)
-x = Dropout(.2)(x)
-x = BatchNormalization()(x)
-dataset_embed = Dense(units=50, activation='relu', name='Image_Embed')(x)
-x = Dropout(.2)(x)
-
-# Model properties embedding
-properties_input = keras.Input(shape=(NUM_OPTIMIZERS + 1))
-properties_embed = Dense(units=50, name='Model_Properties_Embed')(properties_input)
-x = Dropout(.2)(x)
-
-# Combines both models
-merge = Concatenate(axis=1, name="Total_Embed")([dataset_embed, properties_embed])
-
-# learning rate output
-x = Dense(units=100, activation='relu')(merge)
-x = Dropout(.2)(x)
-output = Dense(units=1, name='Learning_Rate', activation='relu')(x)
-
-# This model covers all the inputs and outputs
-model = keras.Model([img_input, properties_input], output)
-
-# Import and preprocess data
-print("Started importing data")
-start = time.time()
-
-#directory = "/kaggle/input/dataset-but-with-sub-dataset-data/" #for kaggle
-directory = "" #for local
-with open(f"{directory}datasets", "rb") as fp:
-    datasets = pickle.load(fp)
+class DatasetGenerator(keras.utils.Sequence):
+    cursor = [0, 0]
     
-with open(f"{directory}model_runs_RUN_0", "rb") as fp:
-    true_rates = pickle.load(fp)
-
-with open(f"{directory}model_runs_RUN_1", "rb") as fp:
-    true_rates.extend(pickle.load(fp))
-    
-with open(f"{directory}model_runs_RUN_2", "rb") as fp:
-    true_rates.extend(pickle.load(fp))
-    
-with open(f"{directory}model_runs_RUN_3", "rb") as fp:
-    true_rates.extend(pickle.load(fp))
-
-
-#Normalize Parameter numbers
-param_num = [i[0] for i in true_rates]
-normalized_param = [(i - min(param_num))/(max(param_num) - min(param_num)) for i in param_num]
-with open("min_max", "wb") as fp:
-    pickle.dump([min(param_num), max(param_num)], fp)
-
-
-#One Hot Encodes the optimizer
-all_optimizers = [i[1] for i in true_rates]
-KNOWN_OPTIMIZERS = ["SGD", "RMSprop", "Adam", "Adadelta", "Adagrad", "Adamax", "Nadam", "Ftrl"]
-encoded = []
-for i in all_optimizers:
-    found = -1
-    for index, j in enumerate(KNOWN_OPTIMIZERS):
-        if i == j:
-            found = index
-            break
-    if found == -1:
-        raise ValueError("Invalid optimizer cannot be One-Hot-Encoded")
-    else:
-        onehot = [0 for _ in KNOWN_OPTIMIZERS]
-        onehot[found] = 1
-        encoded.append(onehot)
-
-#Concatenates encoded with parameter numbers
-assert len(encoded) == len(normalized_param)
-for index, element in enumerate(encoded):
-    element.append(normalized_param[index])
-encoded = np.array(encoded)
-
-#Create y
-y = np.array([i[3] for i in true_rates])
-
-#Match up dataset names to datasets
-dataset_types = [i[2] for i in true_rates]
-#images = np.array([datasets[l] for l in dataset_types])
-encoded2 = []
-y2 = []
-image_chunks = []
-for index, dataset_name in enumerate(tqdm(dataset_types)):
-    dataset = datasets[dataset_name]
-    
-    #This code is disgusting but i'm so tired
-    image_chunks_list = [dataset[0:100], dataset[100:200]]
-    #image_chunks_list = [dataset[0:100]]
-    for image_chunk in image_chunks_list:
-        image_chunks.append(image_chunk)
-        y2.append(y[index])
-        encoded2.append(encoded[index])#continues to append the same row
+    def __init__(self, datasets, dataset_identifiers, subnet_data, y, BATCH_SIZE, DATASET_SEGMENTS, NUM_IMAGES):
+        """Initializes all the necessary variables from ModelTrainer"""
+        self.datasets = datasets
+        self.dataset_identifiers = dataset_identifiers
+        self.subnet_data = subnet_data
+        self.y = y
+        self.BATCH_SIZE = BATCH_SIZE
+        self.DATASET_SEGMENTS = DATASET_SEGMENTS
+        self.NUM_IMAGES = NUM_IMAGES
         
-#to numpy arrays
-encoded2 = np.array(encoded2)
-y2 = np.array(y2)
-image_chunks = np.array(image_chunks)
-
-#shuffle
-assert len(encoded2) == len(y2) == len(image_chunks)
-p = np.random.permutation(len(encoded2))
-encoded2 = encoded2[p]
-y2 = y2[p]
-image_chunks = image_chunks[p]
-
-#Create y & multiplies it by 100 so that the max is 100 and the min is 0.01
-#This is helpful since the rest of the model is dealing with values ranging between 0 and 1
-#NOTE: When doing postprocessing, the value must be divided by 100
-#Lowest metric so far, 0.03 accurcy
-y2 *= 1000
-ratio = round(TRAIN_TEST_SPLIT * len(y2))
-y_train = y2[ratio:]
-y_val = y2[:ratio]
-
-#Create x
-images_train = image_chunks[ratio:]
-encoded_train = encoded2[ratio:]
-images_val = image_chunks[:ratio]
-encoded_val = encoded2[:ratio]
-x_train = [images_train, encoded_train]
-x_val = [images_val, encoded_val]
-
-#Record time
-elapsed = round(time.time() - start, 3)
-print(f"Finished importing data || Elapsed Time: {elapsed}s")
-
-#Tensorboard compatibility
-log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-
-#Generate x dataset
-def generate_x():
+    def __len__(self):
+        """Returns the number of batches per sequence"""
+        return math.ceil((len(self.dataset_identifiers) * self.DATASET_SEGMENTS) / self.BATCH_SIZE)
     
+    def __getitem__(self, idx):
+        """Ignores the index value it's handed and instead uses a cursor to keep track of progress"""
+        dataset_slices = []
+        subnet_instances = [] 
+        y_instances = []
+        counter = 0
+        for index, identifier in enumerate(self.dataset_identifiers[self.cursor[0]:]): #For every dataset unique pair
+            for slice_ in range(self.cursor[1], self.DATASET_SEGMENTS): #For every segment within the dataset
+                dataset = self.datasets[identifier]
+                dataset_slices.append(dataset[slice_ * self.NUM_IMAGES:(slice_ * self.NUM_IMAGES) + self.NUM_IMAGES])
+                subnet_instances.append(self.subnet_data[index])
+                y_instances.append(self.y[index])
+                if counter == self.BATCH_SIZE - 1:
+                    self.cursor[0] = index + 1 if slice_ == self.DATASET_SEGMENTS else index #If the next available segment is in the next dataset, save the cursor on the next dataset and not the current
+                    self.cursor[1] = (slice_ + 1) % self.DATASET_SEGMENTS #without the plus one, it'd reread the current selection, and the modulo prevents it from going out of bounds
+                    return [np.array(dataset_slices), np.array(subnet_instances)], np.array(y_instances)
+                counter += 1
+                
+    def on_epoch_end(self):
+        """Resets the cursor at the end of every epoch"""
+        self.cursor[0] = 0
+        self.cursor[1] = 0
+        
+class ModelTrainer():
+    #directory = "/kaggle/input/dataset-but-with-sub-dataset-data/" #for kaggle
+    directory = "" #for local
+    NUM_IMAGES = 100  # images per timestep
+    TRAIN_TEST_SPLIT = 0.2
+    BATCH_SIZE = 2 #Batch size of 1 will not work for whatever reaosn
+    NUM_OPTIMIZERS = 8 # Number of unique optimizers being used in "Optimized_Parameters.py"
+    DATASET_SEGMENTS = math.floor(config.NUM_IMAGES_MAIN / NUM_IMAGES)
+    
+    def create_model(self):
+        start = time.time()
+        print("Started creating model")
+        
+        #Image embedding
+        img_input = keras.Input(shape=(self.NUM_IMAGES, config.RESOLUTION[0], config.RESOLUTION[1], 1))# shape=(timesteps,resolution,resolution,rgb channels)
+        x = TimeDistributed(Conv2D(filters=8, kernel_size=(3, 3), padding='same', activation='relu'))(img_input) #Full scale model has 64 filters for both
+        x = TimeDistributed(Conv2D(filters=8, kernel_size=(3, 3), padding='same', activation='relu'))(x)
+        x = TimeDistributed(MaxPooling2D(pool_size=(2, 2), strides=2))(x)
+        #filters_convs = [(128, 2), (256, 3), (512, 3), (512, 3)] #Full scale model, my computer doesn't have enough GPU memory for it
+        filters_convs = [(8, 2), (8, 3), (8, 3), (8, 3)] #Reduced scale model, easier on GPU memory
+        for n_filters, n_convs in filters_convs:
+            for _ in range(n_convs):
+                x = TimeDistributed(Conv2D(filters=n_filters, kernel_size=(3, 3), padding='same', activation='relu'))(x)
+            x = TimeDistributed(MaxPooling2D(pool_size=(2, 2), strides=2))(x)
+        x = TimeDistributed(Flatten())(x)
+        x = TimeDistributed(Dense(units=50), name='Image_Preprocessing')(x)
+        x = LSTM(units=50)(x)
+        x = Dropout(.2)(x)
+        x = BatchNormalization()(x)
+        dataset_embed = Dense(units=50, activation='relu', name='Image_Embed')(x)
+        x = Dropout(.2)(x)
 
-#Just add some more preprocessing and a model.fit and you'll be done
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), loss="mse", metrics=[tf.keras.metrics.MeanAbsoluteError()])
-model.fit(x=x_train, y=y_train, epochs=50, batch_size=BATCH_SIZE, validation_data = (x_val, y_val), callbacks=[tensorboard_callback])
+        # Model properties embedding
+        properties_input = keras.Input(shape=(self.NUM_OPTIMIZERS + 1))
+        properties_embed = Dense(units=50, name='Model_Properties_Embed')(properties_input)
+        x = Dropout(.2)(x)
 
-model.save('saved_model/Fleat_model')
+        # Combines both models
+        merge = Concatenate(axis=1, name="Total_Embed")([dataset_embed, properties_embed])
+
+        # learning rate output
+        x = Dense(units=100, activation='relu')(merge)
+        x = Dropout(.2)(x)
+        output = Dense(units=1, name='Learning_Rate', activation='relu')(x)
+
+        # This model covers all the inputs and outputs
+        model = keras.Model([img_input, properties_input], output)
+        
+        elapsed_time = time.time() - start
+        print(f"Finished creating model || Elapsed time: {elapsed_time}s")
+        return model
+
+    def import_data(self):
+        """Imports all the data necessary for creating x and y"""
+        print("Started importing data")
+        start = time.time()
+        
+        with open(f"{self.directory}datasets", "rb") as fp:
+            datasets = pickle.load(fp)
+            
+        with open(f"{self.directory}model_runs_RUN_0", "rb") as fp:
+            true_rates = pickle.load(fp)
+
+        with open(f"{self.directory}model_runs_RUN_1", "rb") as fp:
+            true_rates.extend(pickle.load(fp))
+            
+        with open(f"{self.directory}model_runs_RUN_2", "rb") as fp:
+            true_rates.extend(pickle.load(fp))
+            
+        with open(f"{self.directory}model_runs_RUN_3", "rb") as fp:
+            true_rates.extend(pickle.load(fp))
+        
+        elapsed_time = time.time() - start
+        print(f"Finished importing data || Elapsed time: {elapsed_time}s")
+        
+        return datasets, true_rates
+
+    def get_params(self, true_rates):
+        """Takes out the parameter column, normalizes those values, and save min & max for use in the fleat library itself"""
+        param_num = [i[0] for i in true_rates]
+        normalized_param = [(i - min(param_num))/(max(param_num) - min(param_num)) for i in param_num]
+        with open("min_max", "wb") as fp:
+            pickle.dump([min(param_num), max(param_num)], fp)
+        return normalized_param
+
+    def get_optimizers(self, true_rates):
+        """Takes the optimizer column out of true_rates and One Hot Encodes the optimizer"""
+        all_optimizers = [i[1] for i in true_rates]
+        KNOWN_OPTIMIZERS = ["SGD", "RMSprop", "Adam", "Adadelta", "Adagrad", "Adamax", "Nadam", "Ftrl"]
+        encoded = []
+        for i in all_optimizers:
+            found = -1
+            for index, j in enumerate(KNOWN_OPTIMIZERS):
+                if i == j:
+                    found = index
+                    break
+            if found == -1:
+                raise ValueError("Invalid optimizer cannot be One-Hot-Encoded")
+            else:
+                onehot = [0 for _ in KNOWN_OPTIMIZERS]
+                onehot[found] = 1
+                encoded.append(onehot)
+        return encoded
+
+    def get_identifiers(self, true_rates):
+        """Takes the dataset identifiers out of true_rates"""
+        return [i[2] for i in true_rates]
+    
+    def get_y(self, true_rates):
+        """Takes the y column out of true_rates & multiplies it by 100 so that the max is 100 and the min is 0.01"""
+        SCALE_FACTOR = 1000
+        y = [i[3] for i in true_rates]
+        y = [i * SCALE_FACTOR for i in y]
+        return y
+
+    def concat_vals(self, x, y):
+        """Concatenate any two lists together"""
+        assert len(x) == len(y)
+        concat = []
+        for index in range(0, len(x)):
+            temp = [x[index]]
+            for i in y[index]:
+                temp.append(i)
+            concat.append(temp)
+        return np.array(concat)
+    
+    def train_model(self):
+        """Trains and saves model"""
+        model = self.create_model()
+        datasets, true_rates = self.import_data()
+        
+        #Time preprocessing
+        print("Started preprocessing data")
+        start = time.time()
+        
+        #Shuffle dataset
+        self.shuffle(true_rates)
+        
+        #Extract and preprocess all the elements of true_rates
+        param_num = self.get_params(true_rates)
+        optimizers = self.get_optimizers(true_rates)
+        dataset_identifiers = self.get_identifiers(true_rates)
+        y = self.get_y(true_rates)
+        
+        #Merges param_num and optimizers to form the total data on the sub net
+        subnet_data = self.concat_vals(param_num, optimizers)
+        
+        #Creates train test split for all datasetst
+        identifiers_train, identifiers_test = self.train_test_split(dataset_identifiers)
+        subnet_train, subnet_test = self.train_test_split(subnet_data)
+        y_train, y_test = self.train_test_split(y)
+        
+        #Time preprocessing
+        elapsed_time = time.time() - start
+        print(f"Finished preprocessing data || Elapsed time: {elapsed_time}s")
+        
+        #Tensorboard compatibility
+        log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+        
+        #Just add some more preprocessing and a model.fit and you'll be done
+        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), loss="mse", metrics=[tf.keras.metrics.MeanAbsoluteError()])
+        train_generator = DatasetGenerator(datasets, identifiers_train, subnet_train, y_train, self.BATCH_SIZE, self.DATASET_SEGMENTS, self.NUM_IMAGES)
+        test_generator = DatasetGenerator(datasets, identifiers_test, subnet_test, y_test, self.BATCH_SIZE, self.DATASET_SEGMENTS, self.NUM_IMAGES)
+        model.fit(train_generator,
+                  epochs=10,
+                  batch_size=self.BATCH_SIZE,
+                  validation_data = test_generator,
+                  callbacks=[tensorboard_callback])
+        
+        model.save('saved_model/Fleat_model')
+
+    def shuffle(self, a):
+        """Shuffles a list"""
+        random.shuffle(a)
+
+    def train_test_split(self, a):
+        """All inputs should not be duplicated using DATASET_SEGMENTS"""
+        ratio = round(self.TRAIN_TEST_SPLIT * len(a))
+        a_train = a[ratio:]
+        a_test = a[:ratio]
+        return a_train, a_test
+
+# This way, imports can use the class w/o running the script
+if __name__ == "__main__":
+    model_trainer = ModelTrainer()
+    model_trainer.train_model()
